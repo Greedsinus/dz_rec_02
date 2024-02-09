e92400d1d9f5aac4df5be1f41ec2576b895ae840
Алексей Шепелев <distort9@mail.ru>
2024-02-10 01:25:48 +0300

    all dz

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+System.Console.WriteLine("enter u number");
+int n = Convert.ToInt32(Console.ReadLine()); //задал переменную с точным названием из задания, мне так было удобнее
+System.Console.WriteLine("enter u number");
+int m = Convert.ToInt32(Console.ReadLine()); //задал переменную с точным названием из задания, мне так было удобнее
+
+int NaturalNumbers(int min, int max) //функция, создаем
+{
+    if (min > max)
+    {
+        return 0;   //выход из рекурсии
+    }
+
+    System.Console.Write(min + " "); //вот тут мне было удобнее сделать вывод, в строку с разделением
+
+    return NaturalNumbers(min +1, max); //собственно решение + рекурсия
+
+} 
+
+NaturalNumbers(n, m); //вызвал функцию
---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 2; //создал и задал небольшое значение (формула тяжелая)
+int number2 = 3; //создал и задал небольшое значение (формула тяжелая)
+
+int Ackermann(int m, int n) //собственно функция - принимает 2 целых аргумента
+    {
+        if (m == 0) //проверяем на равеновство и первое вхождение
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0) //доп. условие
+        {
+            return Ackermann(m - 1, 1); //еще одно
+        }
+        else // m > 0 && n > 0
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+System.Console.WriteLine(Ackermann(number,number2)); //вывод и вызов функции
